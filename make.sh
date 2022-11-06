7a6ebfadbb26f26c299578e672cbaa56d67668ba
Keita Mizukoshi <mizukoshi.keita@jaxa.jp>
2022-11-06 14:31:39 +0900

    Add new build system

--- make.sh
+++ make.sh
@@ -8,4 +8,4 @@ python3 setup.py bdist_wheel
 echo Done.
 echo local install
 echo 'python3 -m pip install dist/glgl-*.whl --upgrade'
-python -m pip uninstall glgl && python -m pip install dist/glgl-0.1.0-py3-none-any.whl
+#python -m pip uninstall glgl && python -m pip install dist/glgl-0.1.0-py3-none-any.whl
